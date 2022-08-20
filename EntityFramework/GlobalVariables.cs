173219a58bfc59c2f73ad43d0a0fa3bda3252682
pobbatti vijaya lakshmi <honeyvijji643@gmail.com>
2022-08-20 20:41:27 +0530

    Add project files.

---
+++ EntityFramework/GlobalVariables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace EntityFramework
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient WebAPIClient = new HttpClient();
+
+        static GlobalVariables()
+        {
+
+            WebAPIClient.BaseAddress = new Uri("https://localhost:44304/api/");
+            WebAPIClient.DefaultRequestHeaders.Clear();
+            WebAPIClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+
+            
+    }
+}
